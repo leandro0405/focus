3b1f7f54f9dba89ea16c2807bc141becc51cd150
leandro0405 <162132097+leandro0405@users.noreply.github.com>
2026-08-10 08:28:40 -0300

    Refactor visibilitychange event listener syntax

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
-document.addEventListener("visibilitychange", () => {
+// ==============================
+// CONTROLE DA ANIMAÇÃO
+// ==============================
+
+document.addEventListener("visibilitychange", function () {
 
     if (!document.hidden) {
 
         const background = document.querySelector(".background");
         const title = document.querySelector("h1");
 
+        // Reinicia a animação
+
         background.style.animation = "none";
         title.style.animation = "none";
 
-        requestAnimationFrame(() => {
+        requestAnimationFrame(function () {
 
             background.style.animation = "";
             title.style.animation = "";
